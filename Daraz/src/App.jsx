d8f553c38ee21b212d8c906d9ba4e345cfdf1b3f
Zaheer Ali <zaheercode09@gmail.com>
2025-12-30 13:22:10 +0500

    daraz app

---
+++ Daraz/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import { FaShoppingCart } from "react-icons/fa";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+<FaShoppingCart size={22} />
+    </>
+  )
+}
+
+export default App
